f2adf8d131cd7a669c2d5beb2cc382cfe64d3ca0
zjcf <zhoujie@wrdtech.com>
2021-01-29 00:09:32 +0800

    [feature] 调整二级菜单权限控制

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -30,10 +30,12 @@
                     @php
                         $item['depth'] = $depth + 1;
                     @endphp
-                    <a @if(mb_strpos($item['uri'], '://') !== false) target="_blank" @endif href="{{ $builder->getUrl($item['uri']) }}" class="nav-link {!! $builder->isActive($item) ? 'active' : '' !!}">
-                        {!! str_repeat('&nbsp;', $depth) !!}<i class="fa {{ $item['icon'] ?: 'feather icon-circle' }}"></i>
-                            {{ $builder->translate($item['title']) }}
-                    </a>
+		    @if($builder->visible($item))
+                    	<a @if(mb_strpos($item['uri'], '://') !== false) target="_blank" @endif href="{{ $builder->getUrl($item['uri']) }}" class="nav-link {!! $builder->isActive($item) ? 'active' : '' !!}">
+                    	    {!! str_repeat('&nbsp;', $depth) !!}<i class="fa {{ $item['icon'] ?: 'feather icon-circle' }}"></i>
+                    	        {{ $builder->translate($item['title']) }}
+                    	</a>
+		    @endif
                 @endforeach
             </div>
         </li>
