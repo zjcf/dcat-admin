619c4b97dd088e4fedcdd27587c2431a5e71e322
zjcf <zhoujie@wrdtech.com>
2021-02-22 23:49:20 +0800

    [feature] 快速搜索增加提交按钮

--- resources/views/grid/quick-search.blade.php
+++ resources/views/grid/quick-search.blade.php
@@ -1,6 +1,9 @@
 <style>::-ms-clear,::-ms-reveal{display: none;}</style>
 
 <form action="{!! $action !!}" class="input-no-border d-md-inline-block" pjax-container style="display:none;margin-right: 16px">
+    <button class="btn btn-primary filter-btn-lfkLKWCT btn-outline" type="submit">
+        <i class="feather icon-search"></i><span class="d-none d-sm-inline">&nbsp;&nbsp;搜索</span>
+    </button>
     <div class="table-filter">
         <label style="width: {{ $width }}rem">
             <input
